63c0d63e4973e21a162f17e534680c84e22d5de4
Hiren <dev01@gmail.com>
2026-07-03 11:16:59 +0530

    update_translation change

--- update_translation.dart
+++ update_translation.dart
@@ -8,6 +8,12 @@ void main() {
   final enMap = jsonDecode(enFile.readAsStringSync());
   final guMap = jsonDecode(guFile.readAsStringSync());
   
+  // Add to English
+  enMap['Edit profile request sent'] = 'Edit profile request sent';
+
+  // Add to Gujarati
+  guMap['Edit profile request sent'] = 'પ્રોફાઇલ સંપાદન વિનંતી મોકલાઈ ગઈ છે';
+
   enMap['Notifications'] = 'Notifications';
   enMap['No notifications found'] = 'No notifications found';
 
